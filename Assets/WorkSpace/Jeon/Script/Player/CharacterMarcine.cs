c57da6fee654264a6fff1deb4295759f218a0512
gull295 <hitaeyeun@gmail.com>
2024-12-27 17:58:46 +0900

    추가

---
+++ Assets/WorkSpace/Jeon/Script/Player/CharacterMarcine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMarcine : MonoBehaviour
+{
+    [SerializeField] float HP;
+    [SerializeField] float MoveSpeed;
+
+    protected Rigidbody2D rigidbody2D;
+    protected Animator animator;
+    protected Weapon playerWeapon;
+    protected CharacterState currentBState;
+
+
+    public Vector2 currentDir { get; protected set;}
+    public Rigidbody2D Rigidbody2D => rigidbody2D;
+
+    public void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    public void Move()
+    {
+        rigidbody2D.velocity = currentDir * MoveSpeed;
+    }
+    public void TakeDamage()
+    {
+
+    }
+    public void ChangePlayerState(CharacterState newState)
+    {
+        currentBState?.Exit();
+        currentBState = newState;
+        currentBState.Enter();
+    }
+
+    public void SetDir(Vector2 vector2) => currentDir = vector2;
+}
