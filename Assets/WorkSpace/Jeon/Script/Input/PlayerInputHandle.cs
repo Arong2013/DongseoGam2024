bafd979b96c9b34b85da63fb60d49ef2b46229f0
gull295 <hitaeyeun@gmail.com>
2024-12-28 17:45:00 +0900

    업데이트

--- Assets/WorkSpace/Jeon/Script/Input/PlayerInputHandle.cs
+++ Assets/WorkSpace/Jeon/Script/Input/PlayerInputHandle.cs
@@ -7,8 +7,7 @@ public class PlayerInputHandle : MonoBehaviour, IPlayerUesableUI
     PlayerMarcine characterMarcine;
     public bool IsinputAble;
     public GameObject uiPanel;
-
-    // 게임이 일시 정지되었는지 여부
+    public bool isBtnUp;
     private bool isPaused = false;
 
     public void Initialize(PlayerMarcine playerMarcine)
@@ -41,14 +40,16 @@ public class PlayerInputHandle : MonoBehaviour, IPlayerUesableUI
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
 
-            if (Input.GetMouseButton(0) && characterMarcine.isAttackAble)
+            if (Input.GetMouseButton(0) && characterMarcine.isAttackAble && isBtnUp)
             {
+                isBtnUp = false;
                 Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector2 objectPosition = characterMarcine.transform.position;
                 Vector2 direction = mousePosition - objectPosition;
                 characterMarcine.SetAttackAngle(direction);
                 characterMarcine.SetAnimatorValue(CharacterAnimeBoolName.CanAttack, true);
             }
+            if(Input.GetMouseButtonUp(0) && characterMarcine.isAttackAble) isBtnUp = true;
             characterMarcine.Walkable = Mathf.Abs(moveHorizontal) > 0.2 || Mathf.Abs(moveVertical) > 0.2;
             characterMarcine.SetDir(new Vector2(moveHorizontal, moveVertical));
         }
--- Assets/WorkSpace/Jeon/Script/Player/Thron.cs
+++ Assets/WorkSpace/Jeon/Script/Player/Thron.cs
@@ -4,23 +4,13 @@ using UnityEngine.UI;
 
 public class Thron : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.GetComponent<PlayerMarcine>())
-        {
-            // Implement any behavior for when the player enters the trigger zone, if needed.
-        }
-    }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.TryGetComponent<PlayerMarcine>(out PlayerMarcine playerMarcine))
         {
-            // Apply damage to the player
             playerMarcine.TakeDamage(1f);
-            Vector2 forceDirection = (collision.transform.position - transform.position).normalized; // Direction away from the object
-            float forceMagnitude = 5f; // Adjust the force magnitude as needed
-            playerMarcine.Rigidbody2D.AddForce(forceDirection * forceMagnitude, ForceMode2D.Impulse); // Apply an impulse force
+          
         }
     }
 }
